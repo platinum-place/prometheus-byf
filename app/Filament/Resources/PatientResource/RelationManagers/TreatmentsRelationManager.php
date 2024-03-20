70ad88e57fad2375167a82aca0e9149af6cbf004
Warlyn <warlyn.garcia@bitboxonline.com>
2024-03-20 15:16:26 -0400

    example filament

---
+++ app/Filament/Resources/PatientResource/RelationManagers/TreatmentsRelationManager.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources\PatientResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Columns\Summarizers\Average;
+use Filament\Tables\Columns\Summarizers\Range;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\Summarizers\Sum;
+use Filament\Tables\Columns\Summarizers\Count;
+
+class TreatmentsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'treatments';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('description')
+                    ->required()
+                    ->maxLength(255)
+                    ->columnSpan('full'),
+                Forms\Components\Textarea::make('notes')
+                    ->maxLength(65535)
+                    ->columnSpan('full'),
+                Forms\Components\TextInput::make('price')
+                    ->numeric()
+                    ->prefix('RD')
+                    ->maxValue(42949672.95),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('description')
+            ->columns([
+                Tables\Columns\TextColumn::make('description'),
+                Tables\Columns\TextColumn::make('price')
+                    ->money('DOP')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('price')
+                    ->numeric()
+                    ->summarize(Sum::make()->label('Total'))
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -49,6 +49,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return /*str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail()*/ true;
+        return /* str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail() */ true;
     }
 }
